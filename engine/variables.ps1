fa6da8e6a62279c773be9d0633aeb049cd872971
My Random Thoughts <support@myrandomthoughts.co.uk>
2016-10-28 15:38:02 +0100

    No longer required

--- engine/variables.ps1
+++
@@ -1,8 +0,0 @@
-﻿[string]   $logoName              = 'ATOS'                            # Your company name, used for report titles
-
-[int]      $script:ccTasks        =   5                               # Number of concurrent tasks to perform (the higher the number the more resources you need)
-[int]      $script:waitTime       = 100                               # Time to wait between starting new tasks (milliseconds)
-[int]      $script:checkTimeout   =  60                               # Time to wait for each task to complete (seconds)
-[string]   $script:qaOutput       = "$env:SystemDrive\QA\Results\"    # Report output location
-[hashtable]$script:lang           = @{}                               # Holds language strings
-[hashtable]$script:qahelp         = @{}                               # Holds help text for the HTML report
